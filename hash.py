ea01fd411a27dbfd5a9cd12b8b22629bea241f91
AnshumanNeon <anshumanneon@gmail.com>
2025-06-11 11:50:54 +0530

    add comments to the hash file

--- hash.py
+++ hash.py
@@ -1,10 +1,14 @@
 import os
 from cryptography.hazmat.primitives.kdf.argon2 import Argon2id
 
+# hash the password without using a salt
+# since the function requires a salt of an appropriate length. we pass a common
+# salt "0000000000000000" after converting it to bytes.
 def hash_without_salt(password):
     kdf = Argon2id(iterations = 1, lanes=4, memory_cost = 64*64, salt=('0' * 16).encode("utf-8"), length=16)
     return kdf.derive(password)
 
+# hash the password using a custom salt
 def hash_with_salt(password, salt):
     kdf = Argon2id(iterations = 1, lanes=4, memory_cost = 64*64, salt=salt, length=16)
     return kdf.derive(password)
